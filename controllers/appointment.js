d1636b723374d20c83d63aadb9280617169065a5
cbcrisel <cristhian7096@gmail.com>
2022-06-12 15:10:50 -0400

    appointment with date fixed

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -5,9 +5,9 @@ const {pool}= require('../database/config');
 
 const postAppointment= async(req=request,res=response)=>{
     try {
-        const {descripcion,id_turno,id_user,id_esteticista,id_servicio}=req.body;
+        const {descripcion,id_turno,fecha,id_user,id_esteticista,id_servicio}=req.body;
         const id_cliente = await pool.query(`SELECT cliente.id_persona as id_cliente FROM cliente,persona,usuario WHERE usuario.id_persona=persona.id and cliente.id_persona=persona.id and usuario.id=${id_user}`);
-        const query = await pool.query(`INSERT INTO cita(descripcion,fecha,id_turno,id_cliente,id_esteticista,id_servicio) VALUES('${descripcion}',CURRENT_DATE,${id_turno},${id_cliente.rows[0].id_cliente},${id_esteticista},${id_servicio})`);
+        const query = await pool.query(`INSERT INTO cita(descripcion,fecha,id_turno,id_cliente,id_esteticista,id_servicio) VALUES('${descripcion}','${fecha}',${id_turno},${id_cliente.rows[0].id_cliente},${id_esteticista},${id_servicio})`);
         res.json({
             message: 'Cita creada',
             body: {
