04d8664b4643dff91a91c2df1087c14706ea6223
cbcrisel <cristhian7096@gmail.com>
2022-06-27 20:23:15 -0400

    id_esteticista fixed

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -50,15 +50,13 @@ const getAppointmentsOfAnUser= async(req=request,res=response)=>{
         res.json({message:error});
     }
 }
-
 const getAppointmentsOfAnEsteticista=async(req=request, res=response)=>{
     try {
         const {id_user}=req.body
         const id_esteticista= await pool.query(`SELECT esteticista.id_persona as id_esteticista FROM esteticista,persona,usuario WHERE usuario.id_persona=persona.id and esteticista.id_persona=persona.id and usuario.id=${id_user}`)
-        console.log('hola',id_esteticista.rows[0].id_esteticista);
         const query = await pool.query(`SELECT c.id,c.descripcion,c.fecha, t.hora_inicio, s.nombre 
                                         FROM cita as c,turno as t,servicio as s,turno_servicio as ts
-                                        WHERE c.id_servicio=ts.id_servicio and ts.id_servicio=s.id and c.id_turno=ts.id_turno and ts.id_turno=t.id and c.id_cliente=${id_esteticista.rows[0].id_esteticista}`);
+                                        WHERE c.id_servicio=ts.id_servicio and ts.id_servicio=s.id and c.id_turno=ts.id_turno and ts.id_turno=t.id and c.id_esteticista=${id_esteticista.rows[0].id_esteticista}`);
         res.json({
             message: 'Citas encontradas',
             body: {
