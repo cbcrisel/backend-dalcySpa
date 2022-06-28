db3025f2b1061c7174b4a14668736dd9cde6d2a9
cbcrisel <cristhian7096@gmail.com>
2022-06-28 18:05:45 -0400

    ssl connection

--- database/config.js
+++ database/config.js
@@ -1,11 +1,14 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'dalcyspa',
-    port: '5432'
+    host: 'ec2-18-204-142-254.compute-1.amazonaws.com',
+    user: 'hxcwnrhyvsdmmc',
+    password: 'c903af0ccab79a0b5230450d092c27e342c09e60045cb9e9408f20c7c48c32d1',
+    database: 'dcj4h6maqchj12',
+    port: '5432',
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 module.exports={
