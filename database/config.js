0bce792ab13eeb1e7d10472e2b4a3144223c58a6
cbcrisel <cristhian7096@gmail.com>
2023-04-29 15:00:07 -0400

    env database credentials

--- database/config.js
+++ database/config.js
@@ -1,15 +1,17 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: 'ec2-18-204-142-254.compute-1.amazonaws.com',
-    user: 'hxcwnrhyvsdmmc',
-    password: 'c903af0ccab79a0b5230450d092c27e342c09e60045cb9e9408f20c7c48c32d1',
-    database: 'dcj4h6maqchj12',
-    port: '5432',
-    ssl: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+/*     ssl: {
         rejectUnauthorized: false
-      }
+      } */
 });
+
+
 
 module.exports={
     pool
